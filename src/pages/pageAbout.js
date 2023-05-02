22a4f839a7655d7a5998efc29a0563e7faadadaf
Serge V <prkarpi@hotmail.com>
2023-05-02 13:20:51 -0600

    Updated Text/UI

--- src/pages/pageAbout.js
+++ src/pages/pageAbout.js
@@ -10,7 +10,7 @@ const About = ()  => {
            
             <div className="about-page-paragraph">
             Hello! Shalom! Hola! Ciao! Привіт! Konnichiwa! Привет! Assalamu alaikum! Nǐ hǎo! Shikamoo! 
-            <br/><br/>I am Serge. I am a Fullstack Developer residing in Denver, Colorado. I am passionate about developing software for the web, traveling, writing music, automotive techologies, photography and culture. Beginning in my teenage years I grew fond of computers and after reading a few thick books I found my first job creating websites writing HTML, CSS and PHP. Today I am still passionate about developing software for the web using modern techologies. I enjoy seeing my creativity come to life wether it is music, photography or developing software.
+            <br/><br/>I am Serge. I am a Fullstack Developer residing in Denver, Colorado. I am passionate about developing software for the web, traveling, writing music, automotive technologies, photography and culture. Beginning in my teenage years, I grew fond of computers and after reading a few thick books I found my first job creating websites writing HTML, CSS and PHP. Today I am still passionate about developing software for the web using modern technologies. I enjoy seeing my creativity come to life whether it is music, photography or developing software.
             <div className="about-page-paragraph-techologies"><b>Techologies and Stacks</b><hr/>
             <div className='about-page-paragraph-techologies-stack'>
                 <ul>
--- src/pages/pageEducation.js
+++ src/pages/pageEducation.js
@@ -13,10 +13,10 @@ const Education = () => {
         Attended and received a Bachelor's degree in Arts. Graduated with honors and an overall gpa of 3.4.
         <br/>
         <div className='company-background'>@Self-discipline, self-lerner</div><br/>
-        Beginning with an HTML book in the 2000's I have never quit the discipline of learning. My passion for software engineering grew from a basic HTML coding to a complex JavaScript development. Starting with PHP as my first backend environment it grew into Ruby on Rails. After that React and Node has taken a huge part of my interest and my primary to go tools. Today I am a JavaScript developer. 
+        Beginning with an HTML book in the 2000's, I have never quit the discipline of learning. My passion for software engineering grew from basic HTML coding to complex JavaScript development. Starting with PHP as my first backend environment, it grew into Ruby on Rails. After that, React and Node have became a huge part of my interest and my primary to-go tools. Today I am a JavaScript developer. 
         <br/>
         <div className='company-background'>@Davinci Coders</div><br/>
-        Attended and received a Ruby on Rails certification.
+        Attended and received a Ruby on Rails certificate.
         <br/>
         <div className='company-background'>@Continuing Education</div><br/>
         I am always striving to learn and conquer new technologies as much as needed to stay competitive.
