74ce78e090d030d9a5e6da201fbcaf231ceb6e2b
Serge V <prkarpi@hotmail.com>
2024-03-30 19:12:03 -0600

    linked in link fixed

--- src/pages/pageContact.js
+++ src/pages/pageContact.js
@@ -17,7 +17,7 @@ const Contact = () => {
         <div className="page-header">Contact</div>
            
         <div className="about-page-paragraph">
-            <Link to={'https://www.linkedin.com/in/sergey-vasfield-81055b42/'}><img src={LinkedInImg} alt='LinkedIn' style={{ width: '6em'}}></img></Link>
+            <Link to={'https://www.linkedin.com/in/sergey-vassiliev-81055b42/'}><img src={LinkedInImg} alt='LinkedIn' style={{ width: '6em'}}></img></Link>
             <Link to='email' onClick={handleEmailClick}><img src={EmailImg} alt='LinkedIn' style={{ width: '6em'}}></img></Link>
             <Link to={'https://github.com/prkarpi'}><img src={GitHubImg} alt='LinkedIn' style={{ width: '5.5em', paddingLeft: '.5em'}}></img></Link>
         </div>
