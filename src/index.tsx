36175ca7259834ed9db9ff1926f49863e6a4dd0a
Alex <prkarpi@hotmail.com.com>
2026-02-10 04:14:40 +0000

    more updates on index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,4 @@
-//import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import Container from 'react-bootstrap/Container';
 import './styles/styles.css';
 import Routes from './router';
 import reportWebVitals from './reportWebVitals';
@@ -8,13 +6,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
-  
     <Routes/>
-  
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
